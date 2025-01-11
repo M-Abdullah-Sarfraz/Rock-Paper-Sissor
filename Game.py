63bf2360d548efaaa501d7a29316e5865d19a184
Abdullah Sarfraz <mabdullahsarfraz093@gmail.com>
2025-01-11 18:01:36 +0500

    Add players name

--- Game.py
+++ Game.py
@@ -4,46 +4,48 @@ import math, random
 def rock():
     
     if Player == '✊' and computer_input == '✌':
-        print("Player 1 wins")
-        print("Player 1 choose " + Player + "  Computer choose " + computer_input)
+        print(f"{name} : wins")
+        print(f"{name} choose  {Player}  and computer choose {computer_input}" )
+        
     elif Player == '✊' and computer_input == '✋':
-        print("Player 1 loose")
-        print("Player 1 choose " + Player + "  Computer choose " + computer_input)
+        print(f"{name} : loose")
+        print(f"{name} choose  {Player}  and computer choose {computer_input}" )
     else:
         print("GAME DRAW")
-        print("Player 1 choose " + Player + "  Computer choose " + computer_input) 
+        print(f"{name} choose  {Player}  and computer choose {computer_input}" )
 
 
 # Function for scissor input 
 def scissor():
     if Player == '✌' and computer_input == '✊':
-        print("Player 1 wins")
-        print("Player 1 choose " + Player + "  Computer choose " + computer_input)
+        print(f"{name} : wins")
+        print(f"{name} choose  {Player}  and computer choose {computer_input}" )
     elif Player == '✌' and computer_input == '✋':
-        print("Player 1 loose")
-        print("Player 1 choose " + Player + "  Computer choose " + computer_input)
+        print(f"{name} : loose")
+        print(f"{name} choose  {Player}  and computer choose {computer_input}" )
     else:
         print("GAME DRAW")
-        print("Player 1 choose  " + Player + "  Computer choose " + computer_input) 
+        print(f"{name} choose  {Player}  and computer choose {computer_input}" )
 
 
 
 # Function for paper input 
 def paper():
     if Player == '✋' and computer_input == '✊':
-        print("Player 1 wins")
-        print("Player 1 choose " + Player + "  Computer choose " + computer_input)
+        print(f"{name} : wins")
+        print(f"{name} choose  {Player}  and computer choose {computer_input}" )
     elif Player == '✋' and computer_input == '✌':
-        print("Player 1 loose")
-        print("Player 1 choose " + Player + "  Computer choose " + computer_input)
+        print(f"{name} : loose")
+        print(f"{name} choose  {Player}  and computer choose {computer_input}" )
     else:
         print("GAME DRAW")
-        print("Player 1 choose " + Player + "  Computer choose " + computer_input) 
+        print(f"{name} choose  {Player}  and computer choose {computer_input}" )
 
 
 # Taking input from computer and Player
 
 games_input = [ '✊', '✋' , '✌' ]
+name = input("Enter your name! ")
 Player = input("Enter any symbol (✊, ✋ , ✌ )")
 computer_input = random.choice(games_input)
 
